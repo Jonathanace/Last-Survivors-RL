2ef45cff21a5a412f85c0521b0e6320f15e6b3a0
Jonathanace <jonathanace@gmail.com>
2024-04-18 14:50:14 -0700

    add start_stage and exit_stage calls to LastSurvivors environment

--- src/env.py
+++ src/env.py
@@ -21,7 +21,9 @@ from utils import (
     encoder_dict, 
     # encode_choices,
     quickshow,
-    check_if_choices
+    check_if_choices,
+    start_stage,
+    exit_stage
     )
 import cv2
 from tensordict import TensorDict
@@ -29,9 +31,16 @@ from typing import Optional
 
 
 class LastSurvivors(EnvBase):
-    def __init__(self, *args, **kwargs):
-        validate_icons()
+    def __init__(self, hero, stage, difficulty, level, speed, *args, **kwargs):
+        
+        self.hero = hero
+        self.stage = stage
+        self.difficulty = difficulty
+        self.level = level
+        self.speed = speed        
+
         super().__init__(*args, **kwargs)
+        validate_icons()
         self.action_spec = BoundedTensorSpec(
             low=1,
             high=5,
@@ -46,18 +55,22 @@ class LastSurvivors(EnvBase):
         self.reward_spec = UnboundedContinuousTensorSpec(1, dtype=torch.float32)
 
     def _reset(self, tensordict=None):
+        start_stage(hero=self.hero,
+            stage=self.stage,
+            difficulty=self.difficulty,
+            level=self.level,
+            speed=self.speed)
         sc = screenshot()
-        while not get_choices(sc, quiet=True) and not check_game_end(sc) is False:
+        while not get_choices(sc, quiet=True) and not check_game_end(sc):
             print("Checking frame...")
             sc = screenshot()
 
-        print("Choices Detected")
-        
-        self.choice_names = get_choices(sc)
-
         if check_game_end(sc):
             print("Game has ended. You may want to restart the game manually before beginning training.")
-        
+        elif get_choices(sc):
+            self.choice_names = get_choices(sc)
+        else:
+            raise Exception("Something went wrong.")
         choices = torch.tensor([encoder_dict[choice] for choice in self.choice_names], dtype=torch.float32)
         reward = torch.tensor(0, dtype=torch.float32, requires_grad=True)
         done = torch.tensor(False, dtype=torch.bool)
@@ -89,6 +102,9 @@ class LastSurvivors(EnvBase):
             choices = torch.tensor([-1, -1, -1, -1], dtype=torch.float32)
             reward = torch.tensor(check_win_or_loss(sc), dtype=torch.float32, requires_grad=True)
             done = torch.tensor(True, dtype=torch.bool)
+            pag.click('images/templates/menu/confirm_button.png')
+            exit_stage(3)
+            time.sleep(10)
 
         elif check_if_choices(sc):
             print("Choices Detected") 
@@ -130,11 +146,12 @@ class LastSurvivors(EnvBase):
 #     r = env.rollout(10)
 
 if __name__ == "__main__":
-    if True:
+    if False:
         base_env = LastSurvivors()
         env = TransformedEnv(base_env, ActionMask())
         env.rollout(5)
         # torch.full(env.observation_spec["choices"].shape, -1, dtype=torch.int64)
         # env.rollout(1)
+    base_env = LastSurvivors()
 
     
